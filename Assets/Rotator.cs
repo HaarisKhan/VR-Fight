9272a2453d1de44e1a666ca5cfa696d2117c2a27
HaarisKhan <haariskhan902@gmail.com>
2016-10-01 18:58:28 -0700

    Initial Commit
    Collision and Trigger events, vibration to Arduino board

---
+++ Assets/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+
+public class Rotator : MonoBehaviour {
+
+	SerialPort serial = new SerialPort("COM6",9600);
+	void Update () {
+
+		if (!serial.IsOpen)
+						serial.Open (); 
+
+		int rotation = int.Parse (serial.ReadLine ());
+		transform.localEulerAngles = new Vector3(0,rotation,0);
+
+		
+
+
+	}
+}
